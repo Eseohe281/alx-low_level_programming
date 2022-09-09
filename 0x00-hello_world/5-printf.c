d5ea19b2ef1e7288d95051a3eca5fa2622b0e2e5
Eseohe <eseohe281@gmail.com>
2022-09-08 17:21:00 -0700

    update my sixth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  * main - print the string in the put function 
  *
  * Description: using the main function
- * this program prints "programing is like building a multilingual puzzle
+ * this program prints "programming is like building a multilingual puzzle
  * return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a place of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
